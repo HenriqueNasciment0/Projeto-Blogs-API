ca7ecd905cca4f608cc035c83f096c93379c5e35
HenriqueNasciment0 <you@example.com>
2022-07-18 16:37:39 -0300

    feat: req07 cria model de Category

---
+++ src/database/models/category.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, DataTypes) => {
+    const Category = sequelize.define('Category', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING,
+        },
+    },
+        {
+            timestamps: false
+        });
+
+    return Category;
+};
